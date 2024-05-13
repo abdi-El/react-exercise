0d86c93479b2dbcb92a587ab1b91cb25f3dfe7ae
ab <abdelilah@alfred.xyz>
2024-05-13 10:02:22 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+/*
+  - Fetch data from the JSONPlaceholder API (https://jsonplaceholder.typicode.com/posts).
+  - Each fetched post has the following structure:
+    {
+      userId: number,
+      id: number,
+      title: string,
+      body: string,
+    }
+  - Display the fetched posts in a responsive layout, ensuring that each row contains three posts.
+*/
+
+function App() {
+  return <></>;
+}
+
+export default App;
